8ffa6846a0a891437eaddf8a6df6e6e928b6bbb7
O-Despo <oliverdesposito@gmail.com>
2024-05-17 19:04:36 -0400

    please vite wrok

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import { resolve } from 'path'
 export default {
   root: resolve(__dirname, 'src'),
   publicDir: "./static",
+    base: "/covid-data-visualizer/",
   build: {
     outDir: '../dist',
     target: 'esnext',
-    base: "/covid-data-visualizer/",
   },
   server: {
     port: 8080
