d2f35185449e2b045872b399a8a1cd4c15cd6266
O-Despo <oliverdesposito@gmail.com>
2024-06-04 01:37:14 -0400

    fixed vite config again

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from 'path'
 
 export default {
   root: resolve(__dirname, 'src'),
-  // base: "/covid-data-visualizer/",
+  base: "/covid-data-visualizer/",
   build: {
     outDir: '../dist',
     target: 'esnext',
