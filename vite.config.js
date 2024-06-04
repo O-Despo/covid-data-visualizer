710ab54ad48a4d7e5ae4c6dc85f4e31c9c1255b4
O-Despo <oliverdesposito@gmail.com>
2024-06-04 01:25:34 -0400

    messed up base in last commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 export default {
   root: resolve(__dirname, 'src'),
   publicDir: "./static",
-  // base: "/covid-data-visualizer/",
+  base: "/covid-data-visualizer/",
   build: {
     outDir: '../dist',
     target: 'esnext',
